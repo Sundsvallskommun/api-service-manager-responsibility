21be9805a94293887302223a0d0323cdc8a339b7
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2026-01-16 10:11:11 +0100

    DRAKEN-3104: Fix sonar issue

--- src/main/java/se/sundsvall/managerresponsibility/service/ManagerResponsibilityService.java
+++ src/main/java/se/sundsvall/managerresponsibility/service/ManagerResponsibilityService.java
@@ -5,8 +5,6 @@ import static org.apache.commons.lang3.StringUtils.isBlank;
 import static se.sundsvall.managerresponsibility.service.mapper.ManagerResponsibilityMapper.toManagerResponsibilityList;
 
 import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import se.sundsvall.managerresponsibility.api.model.ManagerResponsibility;
 import se.sundsvall.managerresponsibility.integration.db.ManagerResponsibilityRepository;
@@ -18,8 +16,6 @@ import se.sundsvall.managerresponsibility.integration.db.ManagerResponsibilityRe
  */
 @Service
 public class ManagerResponsibilityService {
-
-	private static final Logger LOG = LoggerFactory.getLogger(ManagerResponsibilityService.class);
 
 	private final ManagerResponsibilityRepository managerResponsibilityRepository;
 
@@ -35,10 +31,9 @@ public class ManagerResponsibilityService {
 	 */
 	public List<ManagerResponsibility> findByOrgId(final String orgId) {
 		if (isBlank(orgId)) {
-			LOG.debug("findByOrgId called with null or blank orgId, returning empty list");
 			return emptyList();
 		}
-		LOG.debug("Finding manager responsibilities by orgId: {}", orgId);
+
 		return toManagerResponsibilityList(managerResponsibilityRepository.findByOrgId(orgId));
 	}
 
@@ -50,10 +45,9 @@ public class ManagerResponsibilityService {
 	 */
 	public List<ManagerResponsibility> findByPersonId(final String personId) {
 		if (isBlank(personId)) {
-			LOG.debug("findByPersonId called with null or blank personId, returning empty list");
 			return emptyList();
 		}
-		LOG.debug("Finding manager responsibilities by personId: {}", personId);
+
 		return toManagerResponsibilityList(managerResponsibilityRepository.findByPersonId(personId));
 	}
 
@@ -65,10 +59,9 @@ public class ManagerResponsibilityService {
 	 */
 	public List<ManagerResponsibility> findByLoginName(final String loginName) {
 		if (isBlank(loginName)) {
-			LOG.debug("findByLoginName called with null or blank loginName, returning empty list");
 			return emptyList();
 		}
-		LOG.debug("Finding manager responsibilities by loginName: {}", loginName);
+
 		return toManagerResponsibilityList(managerResponsibilityRepository.findByLoginName(loginName));
 	}
 }
